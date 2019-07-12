d4b2e7858ef63a1532d426ff7454ca1c2a563bb7
hassanamin994 <hassanmamin994@gmail.com>
2019-07-12 17:30:32 +0200

    update server

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const express = require('express');
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'build')))
+// frontend routes =========================================================
+app.get('/*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
+})
 
 app.listen(3000, () => {
     console.log('started listening on port 3000')
