86ef03c71337622214c3bc236543d70f31c5e438
hassanamin994 <hassanmamin994@gmail.com>
2019-07-12 17:59:13 +0200

    update

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const path = require('path');
 const express = require('express');
-
+const staticFiles = ['js', 'css', 'img']
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'build')))
 // frontend routes =========================================================
 app.get('/*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'build', req.url))
+    const dest = req.url.indexOf('/static') === 0 ? req.url : 'index.html';
+    res.sendFile(path.resolve(__dirname, 'build', dest))
 })
 
 app.listen(3000, () => {
