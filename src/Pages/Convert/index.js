5b75e552b972c5f79c8604e96654b1280b54b382
hassanamin994 <hassanmamin994@gmail.com>
2019-09-01 04:18:13 +0200

    Dont show convert to enlgish for english articles

--- src/Pages/Convert/index.js
+++ src/Pages/Convert/index.js
@@ -436,25 +436,28 @@ class Convert extends React.Component {
                                     </Button>
                                 </div>
 
-                                <div style={{ width: '90%', margin: '2rem auto', justifyContent: 'flex-end', display: 'flex', alignItems: 'center', color: 'white' }}>
-                                    Convert directly to English
-                                   <div style={{ display: 'inline-block', marginLeft: 20 }}>
-                                        <Switch
-                                            checked={this.props.toEnglish}
-                                            onChange={this.props.setToEnglish}
-                                            onColor="#86d3ff"
-                                            onHandleColor="#2693e6"
-                                            handleDiameter={30}
-                                            uncheckedIcon={false}
-                                            checkedIcon={false}
-                                            boxShadow="0px 1px 5px rgba(0, 0, 0, 0.6)"
-                                            activeBoxShadow="0px 0px 1px 10px rgba(0, 0, 0, 0.2)"
-                                            height={20}
-                                            width={48}
-                                        />
-                                    </div>
-                                </div>
+                                {this.props.article && this.props.article.langCode !== 'en-US' && (
 
+                                    <div style={{ width: '90%', margin: '2rem auto', justifyContent: 'flex-end', display: 'flex', alignItems: 'center', color: 'white' }}>
+                                        Convert directly to English
+                                        <div style={{ display: 'inline-block', marginLeft: 20 }}>
+                                            <Switch
+                                                checked={this.props.toEnglish}
+                                                onChange={this.props.setToEnglish}
+                                                onColor="#86d3ff"
+                                                onHandleColor="#2693e6"
+                                                handleDiameter={30}
+                                                uncheckedIcon={false}
+                                                checkedIcon={false}
+                                                boxShadow="0px 1px 5px rgba(0, 0, 0, 0.6)"
+                                                activeBoxShadow="0px 0px 1px 10px rgba(0, 0, 0, 0.2)"
+                                                height={20}
+                                                width={48}
+                                            />
+                                        </div>
+                                    </div>
+
+                                )}
                                 <Grid.Row>
                                     <Grid.Column width={16}>
                                         <Grid style={{ display: 'flex', justifyContent: 'center', marginBottom: '.2rem' }}>
